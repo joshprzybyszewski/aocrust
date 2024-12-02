34326e2091c98c36c653066f3fa7b5feffb7ea7e
Josh Przybyszewski <josh.prezby@gmail.com>
2024-12-01 21:45:00 -0600

    play around with an iterator instead
    I have no idea what I'm doing

--- src/day1.rs
+++ src/day1.rs
@@ -45,36 +45,39 @@ pub fn part2(input_str: &str) -> usize {
     let mut right = Vec::with_capacity(1000);
 
     read_input(input_str, &mut left, &mut right);
-    let mut sum: usize = 0;
+    let mut right_iter = right.into_iter();
+    let mut current = right_iter.next().expect("must have a first value");
+    let mut next_val = right_iter.next();
 
-    let mut ri = 0;
     let mut num_right: usize = 1;
-
-    for v in left {
-        if v < right[ri] {
-            continue;
-        }
-        if v == right[ri] {
-            sum += v * num_right;
-            continue;
-        }
-        // v is more than the right value. Iterate forward.
-
-        ri += num_right as usize;
-        while ri < right.len() && right[ri] < v {
-            ri += 1;
-        }
-        num_right = 1;
-        while ri + (num_right as usize) < right.len()
-            && right[ri] == right[ri + (num_right as usize)]
-        {
-            num_right += 1;
-        }
-
-        if v == right[ri] {
-            sum += v * num_right;
-        }
+    while current == next_val.expect("should have a next value now") {
+        next_val = right_iter.next();
+        num_right += 1;
     }
 
-    return sum;
+    return left.into_iter().fold(0, |sum, v| {
+        if v < current {
+            return sum;
+        }
+        if v == current {
+            return sum + v * num_right;
+        }
+        // v is more than the right value. Iterate forward.
+        while v > current {
+            if next_val.is_none() {
+                return sum;
+            }
+            current = next_val.expect("checked above");
+            num_right = 0;
+            while !next_val.is_none() && current == next_val.expect("checked") {
+                next_val = right_iter.next();
+                num_right += 1;
+            }
+        }
+
+        if v == current {
+            return sum + v * num_right;
+        }
+        return sum;
+    });
 }
