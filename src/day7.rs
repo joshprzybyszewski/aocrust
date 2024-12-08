e04f295685e4309b4d7fc3c36b978b58807bf861
Josh Przybyszewski <josh.prezby@gmail.com>
2024-12-08 10:56:18 -0600

    take a risk
    use concurrency

--- src/day7.rs
+++ src/day7.rs
@@ -1,3 +1,6 @@
+// See https://docs.rs/tokio/latest/tokio/index.html#feature-flags
+use tokio::runtime::Runtime;
+
 const MAX_LINE_LEN: usize = 13;
 const POWERS_OF_TEN: [u64; MAX_LINE_LEN] = [
     1, // the zero'th element will never be queried
@@ -15,8 +18,11 @@ const POWERS_OF_TEN: [u64; MAX_LINE_LEN] = [
     1000000000000,
 ];
 
-fn check1(target: u64, line: &[u64; MAX_LINE_LEN]) -> bool {
-    return check1_inner(target, line, line[0], 1);
+async fn check1(target: u64, line: [u64; MAX_LINE_LEN]) -> u64 {
+    if check1_inner(target, &line, line[0], 1) {
+        return target;
+    }
+    return 0;
 }
 
 fn check1_inner(target: u64, line: &[u64; MAX_LINE_LEN], cur: u64, index: usize) -> bool {
@@ -44,62 +50,74 @@ fn check1_inner(target: u64, line: &[u64; MAX_LINE_LEN], cur: u64, index: usize)
 }
 
 #[aoc(day7, part1)]
-pub fn part1(input: &str) -> u64 {
-    let input = input.as_bytes();
-    let mut i: usize = 0;
-    let mut sum: u64 = 0;
+pub fn part1(input: &str) -> std::io::Result<u64> {
+    let rt = Runtime::new()?;
+    let mut total = 0;
+    rt.block_on(async {
+        let input = input.as_bytes();
+        let mut i: usize = 0;
+        let mut tasks = tokio::task::JoinSet::new();
 
-    while i < input.len() {
-        let mut target: u64 = 0;
-        // yes, I know simd can do this way faster
-        while input[i] != b':' {
-            // end char is the ":" for the first elem.
-            // b':'  = 58
-            target *= 10;
-            target += (input[i] - b'0') as u64;
-            i += 1;
-        }
-
-        let target: u64 = target;
-
-        // skip past ": "
-        i += 2;
-
-        let mut line: [u64; MAX_LINE_LEN] = [0; MAX_LINE_LEN];
-        let mut l_i = 0;
-
-        loop {
-            // ending char is a space or a newline.
-            // b'\n' = 10
-            // b' '  = 32
-            // b'0'  = 48
-            // we only expect digits in this loop
-            while input[i] >= b'0' {
-                line[l_i] *= 10;
-                line[l_i] += (input[i] - b'0') as u64;
+        while i < input.len() {
+            let mut target: u64 = 0;
+            // yes, I know simd can do this way faster
+            while input[i] != b':' {
+                // end char is the ":" for the first elem.
+                // b':'  = 58
+                target *= 10;
+                target += (input[i] - b'0') as u64;
                 i += 1;
             }
-            l_i += 1;
 
-            if input[i] == b'\n' {
-                // skip past the newline
+            let target: u64 = target;
+
+            // skip past ": "
+            i += 2;
+
+            let mut line: [u64; MAX_LINE_LEN] = [0; MAX_LINE_LEN];
+            let mut l_i = 0;
+
+            loop {
+                // ending char is a space or a newline.
+                // b'\n' = 10
+                // b' '  = 32
+                // b'0'  = 48
+                // we only expect digits in this loop
+                while input[i] >= b'0' {
+                    line[l_i] *= 10;
+                    line[l_i] += (input[i] - b'0') as u64;
+                    i += 1;
+                }
+                l_i += 1;
+
+                if input[i] == b'\n' {
+                    // skip past the newline
+                    i += 1;
+                    break;
+                }
                 i += 1;
-                break;
             }
-            i += 1;
+
+            let line: [u64; MAX_LINE_LEN] = line;
+            tasks.spawn(check1(target, line));
         }
 
-        // TODO do this asynchronously
-        if check1(target, &line) {
-            sum += target;
+        while let Some(res) = tasks.join_next().await {
+            total += res.unwrap();
         }
-    }
+    });
 
-    return sum;
+    return Ok(total);
 }
 
-fn check2(target: u64, line: &[u64; MAX_LINE_LEN], digits: &[usize; MAX_LINE_LEN]) -> bool {
-    return check2_inner(target, line, digits, line[0], 1);
+async fn check2(target: u64, line: [u64; MAX_LINE_LEN], digits: [usize; MAX_LINE_LEN]) -> u64 {
+    if check1_inner(target, &line, line[0], 1) {
+        return target;
+    }
+    if check2_inner(target, &line, &digits, line[0], 1) {
+        return target;
+    }
+    return 0;
 }
 
 fn check2_inner(
@@ -145,59 +163,66 @@ fn check2_inner(
 }
 
 #[aoc(day7, part2)]
-pub fn part2(input: &str) -> u64 {
-    let input = input.as_bytes();
-    let mut i: usize = 0;
-    let mut sum: u64 = 0;
+pub fn part2(input: &str) -> std::io::Result<u64> {
+    let rt = Runtime::new()?;
+    let mut total = 0;
+    rt.block_on(async {
+        let input = input.as_bytes();
+        let mut i: usize = 0;
+        let mut tasks = tokio::task::JoinSet::new();
 
-    while i < input.len() {
-        let mut target: u64 = 0;
-        // yes, I know simd can do this way faster
-        while input[i] != b':' {
-            // end char is the ":" for the first elem.
-            // b':'  = 58
-            target *= 10;
-            target += (input[i] - b'0') as u64;
-            i += 1;
-        }
-
-        let target: u64 = target;
-
-        // skip past ": "
-        i += 2;
-
-        // ending char is a space or a newline.
-        // b'\n' = 10
-        // b' '  = 32
-        // b'0'  = 48
-        // we only expect digits in this loop
-        let mut line: [u64; MAX_LINE_LEN] = [0; MAX_LINE_LEN];
-        let mut digits: [usize; MAX_LINE_LEN] = [0; MAX_LINE_LEN];
-        let mut l_i = 0;
-        loop {
-            while input[i] >= b'0' {
-                digits[l_i] += 1;
-                line[l_i] *= 10;
-                line[l_i] += (input[i] - b'0') as u64;
+        while i < input.len() {
+            let mut target: u64 = 0;
+            // yes, I know simd can do this way faster
+            while input[i] != b':' {
+                // end char is the ":" for the first elem.
+                // b':'  = 58
+                target *= 10;
+                target += (input[i] - b'0') as u64;
                 i += 1;
             }
-            l_i += 1;
 
-            if input[i] == b'\n' {
-                i += 1; // skip the newline.
+            let target: u64 = target;
 
-                break;
+            // skip past ": "
+            i += 2;
+
+            // ending char is a space or a newline.
+            // b'\n' = 10
+            // b' '  = 32
+            // b'0'  = 48
+            // we only expect digits in this loop
+            let mut line: [u64; MAX_LINE_LEN] = [0; MAX_LINE_LEN];
+            let mut digits: [usize; MAX_LINE_LEN] = [0; MAX_LINE_LEN];
+            let mut l_i = 0;
+            loop {
+                while input[i] >= b'0' {
+                    digits[l_i] += 1;
+                    line[l_i] *= 10;
+                    line[l_i] += (input[i] - b'0') as u64;
+                    i += 1;
+                }
+                l_i += 1;
+
+                if input[i] == b'\n' {
+                    i += 1; // skip the newline.
+
+                    break;
+                }
+                i += 1; // skip the space.
             }
-            i += 1; // skip the space.
+
+            let line: [u64; MAX_LINE_LEN] = line;
+            let digits: [usize; MAX_LINE_LEN] = digits;
+            tasks.spawn(check2(target, line, digits));
         }
 
-        if check1(target, &line) || check2(target, &line, &digits) {
-            sum += target;
+        while let Some(res) = tasks.join_next().await {
+            total += res.unwrap();
         }
-    }
+    });
 
-    // 625_178_056_831_523 is too low = 625178056831523
-    return sum;
+    return Ok(total);
 }
 
 #[cfg(test)]
@@ -218,21 +243,21 @@ mod test {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&get_example_input()), 3749);
+        assert_eq!(part1(&get_example_input()).unwrap(), 3749);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&get_example_input()), 11387);
+        assert_eq!(part2(&get_example_input()).unwrap(), 11387);
     }
 
     #[test]
     fn part1_real_input() {
-        assert_eq!(part1(&get_input()), 66343330034722)
+        assert_eq!(part1(&get_input()).unwrap(), 66343330034722)
     }
 
     #[test]
     fn part2_real_input() {
-        assert_eq!(part2(&get_input()), 637696070419031)
+        assert_eq!(part2(&get_input()).unwrap(), 637696070419031)
     }
 }
