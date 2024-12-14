8600a604f2718d836f6779dd6a43c679470fcf34
Josh Przybyszewski <josh.prezby@gmail.com>
2024-12-13 20:30:40 -0600

    solve both parts

--- src/day13.rs
+++ src/day13.rs
@@ -1,15 +1,15 @@
-use std::cmp::max;
+const UNIT_CONVERSION_ERROR: i64 = 10_000_000_000_000;
 
 #[derive(Copy, Clone, Debug)]
 struct Machine {
-    a_x: u32,
-    a_y: u32,
+    a_x: i64,
+    a_y: i64,
 
-    b_x: u32,
-    b_y: u32,
+    b_x: i64,
+    b_y: i64,
 
-    p_x: u32,
-    p_y: u32,
+    p_x: i64,
+    p_y: i64,
 }
 
 impl Machine {
@@ -43,11 +43,11 @@ impl Machine {
         }
 
         *i += 12;
-        machine.a_x += (input[*i] - b'0') as u32;
+        machine.a_x += (input[*i] - b'0') as i64;
         *i += 1;
         while input[*i] != b',' {
             machine.a_x *= 10;
-            machine.a_x += (input[*i] - b'0') as u32;
+            machine.a_x += (input[*i] - b'0') as i64;
             *i += 1;
         }
 
@@ -61,11 +61,11 @@ impl Machine {
         }
         *i += 4;
 
-        machine.a_y += (input[*i] - b'0') as u32;
+        machine.a_y += (input[*i] - b'0') as i64;
         *i += 1;
         while input[*i] != b'\n' {
             machine.a_y *= 10;
-            machine.a_y += (input[*i] - b'0') as u32;
+            machine.a_y += (input[*i] - b'0') as i64;
             *i += 1;
         }
 
@@ -89,11 +89,11 @@ impl Machine {
         }
 
         *i += 13;
-        machine.b_x += (input[*i] - b'0') as u32;
+        machine.b_x += (input[*i] - b'0') as i64;
         *i += 1;
         while input[*i] != b',' {
             machine.b_x *= 10;
-            machine.b_x += (input[*i] - b'0') as u32;
+            machine.b_x += (input[*i] - b'0') as i64;
             *i += 1;
         }
 
@@ -107,11 +107,11 @@ impl Machine {
         }
         *i += 4;
 
-        machine.b_y += (input[*i] - b'0') as u32;
+        machine.b_y += (input[*i] - b'0') as i64;
         *i += 1;
         while input[*i] != b'\n' {
             machine.b_y *= 10;
-            machine.b_y += (input[*i] - b'0') as u32;
+            machine.b_y += (input[*i] - b'0') as i64;
             *i += 1;
         }
 
@@ -132,11 +132,11 @@ impl Machine {
         }
 
         *i += 10;
-        machine.p_x += (input[*i] - b'0') as u32;
+        machine.p_x += (input[*i] - b'0') as i64;
         *i += 1;
         while input[*i] != b',' {
             machine.p_x *= 10;
-            machine.p_x += (input[*i] - b'0') as u32;
+            machine.p_x += (input[*i] - b'0') as i64;
             *i += 1;
         }
 
@@ -150,11 +150,11 @@ impl Machine {
         }
         *i += 4;
 
-        machine.p_y += (input[*i] - b'0') as u32;
+        machine.p_y += (input[*i] - b'0') as i64;
         *i += 1;
         while *i < input.len() && input[*i] != b'\n' {
             machine.p_y *= 10;
-            machine.p_y += (input[*i] - b'0') as u32;
+            machine.p_y += (input[*i] - b'0') as i64;
             *i += 1;
         }
 
@@ -166,43 +166,53 @@ impl Machine {
         return machine;
     }
 
-    fn cost_p1(&self) -> u32 {
+    fn cost_p1(&self) -> i64 {
+        return self.cost::<100>(self.p_x, self.p_y);
+    }
+
+    fn cost_p2(&self) -> i64 {
+        return self.cost::<{ 1 << 62 }>(
+            self.p_x + UNIT_CONVERSION_ERROR,
+            self.p_y + UNIT_CONVERSION_ERROR,
+        );
+    }
+
+    // where's my linear algebra textbook?
+    fn cost<const PRESS_LIMIT: i64>(&self, p_x: i64, p_y: i64) -> i64 {
         // A costs 3 tokens.
         // B costs 1 token.
 
-        let tokens = 0;
+        // num_a * a_x + num_b * b_x                     = p_x
+        // num_a * a_y + num_b * b_y                     = p_y
+        //
+        // 0           + num_b * (b_y * a_x - b_x * a_y) = p_y * a_x - p_x * a_y
+        // num_b = (a_x * p_y - a_y * p_x) / (a_x * b_y  - a_y * b_x)
 
-        let max_b = max(100, max(self.p_x / self.b_x, self.p_y / self.b_y));
-        // let maxA = max(100, max(self.p_x / self.a_x, self.p_y / self.a_y));
-
-        let mut x = self.b_x * max_b;
-        let mut y = self.b_y * max_b;
-
-        for b in (0..max_b).rev() {
-            if self.p_x >= x && self.p_y >= y {
-                let x_diff = self.p_x - x;
-                let y_diff = self.p_y - y;
-                if x_diff % self.a_x == 0 && y_diff % self.a_y == 0 {
-                    let num_b = b;
-                    let num_a = x_diff / self.a_x;
-                    if num_a == y_diff / self.a_y {
-                        return num_b + 1 + num_a * 3;
-                        // println!("machine    = {:?}", self);
-                        // println!("num_b    = {num_b}");
-                        // println!("x_diff   = {x_diff}");
-                        // println!("self.a_x = {}", self.a_x);
-                        // println!("y_diff   = {y_diff}");
-                        // println!("self.a_y = {}", self.a_y);
-                        // println!("num_a    = {num_a}");
-                        // unreachable!();
-                    }
-                }
-            }
-            x -= self.b_x;
-            y -= self.b_y;
+        let numerator_b = self.a_x * p_y - self.a_y * p_x;
+        let denominator_b = self.a_x * self.b_y - self.a_y * self.b_x;
+        if numerator_b % denominator_b != 0 {
+            return 0;
         }
 
-        return 0;
+        let num_b = numerator_b / denominator_b;
+        if num_b > PRESS_LIMIT {
+            return 0;
+        }
+
+        let x_diff = p_x - (num_b * self.b_x);
+        if x_diff % self.a_x != 0 {
+            return 0;
+        }
+        let num_a: i64 = x_diff / self.a_x;
+        if num_a > PRESS_LIMIT {
+            return 0;
+        }
+        if num_a * self.a_y + num_b * self.b_y != p_y {
+            // triple check.
+            return 0;
+        }
+
+        return num_a * 3 + num_b;
     }
 }
 
@@ -221,14 +231,15 @@ fn get_machines(input: &str) -> Vec<Machine> {
 }
 
 #[aoc(day13, part1)]
-pub fn part1(input: &str) -> u32 {
+pub fn part1(input: &str) -> i64 {
     let machines = get_machines(input);
     return machines.iter().map(|machine| machine.cost_p1()).sum();
 }
 
 #[aoc(day13, part2)]
-pub fn part2(input: &str) -> u64 {
-    return 0;
+pub fn part2(input: &str) -> i64 {
+    let machines = get_machines(input);
+    return machines.iter().map(|machine| machine.cost_p2()).sum();
 }
 
 #[cfg(test)]
@@ -259,6 +270,6 @@ mod test {
 
     #[test]
     fn part2_real_input() {
-        assert_eq!(part2(&get_input()), 0)
+        assert_eq!(part2(&get_input()), 90798500745591)
     }
 }
