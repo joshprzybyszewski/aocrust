74619cea83992aa8938fdbcbe2961b3208d6d06c
Josh Przybyszewski <josh.prezby@gmail.com>
2024-12-21 14:28:33 -0600

    closer

--- src/day21.rs
+++ src/day21.rs
@@ -17,7 +17,7 @@
 
 */
 
-const SHORTEST_KEYBOARD_PATHS: [[[usize; MAX_PATH_LENGTH]; NUM_ARROWS]; NUM_ARROWS] =
+const SHORTEST_ARROW_PATHS: [[[usize; MAX_PATH_LENGTH]; NUM_ARROWS]; NUM_ARROWS] =
     generate_shortest_keyboard_paths();
 
 const SHORTEST_NUMERIC_PATHS: [[[usize; MAX_PATH_LENGTH]; NUM_NUMERICS]; NUM_NUMERICS] =
@@ -48,14 +48,12 @@ const NUMERIC_9: usize = 11;
 const NUM_NUMERICS: usize = 12;
 
 struct NumericKeypad {
-    current: usize,
     states: [NumericState; 12],
 }
 
 impl NumericKeypad {
     const fn new() -> Self {
         return NumericKeypad {
-            current: NUMERIC_A,
             states: [
                 // NUMERIC_INVALID,
                 NumericState {
@@ -180,14 +178,12 @@ struct NumericState {
 }
 
 struct ArrowKeypad {
-    current: usize,
     states: [ArrowState; 6],
 }
 
 impl ArrowKeypad {
     const fn new() -> Self {
         return ArrowKeypad {
-            current: ARROW_A,
             states: [
                 // Arrow_INVALID,
                 ArrowState {
@@ -326,13 +322,13 @@ const fn get_shortest_path_between_arrows(start: usize, end: usize) -> [usize; M
     loop {
         let position = pending[index].last();
         if position == end {
-            return pending[pending_index].directions;
+            return pending[index].directions;
         }
 
         if !seen[position] {
             seen[position] = true;
 
-            let mut direction = 1;
+            let mut direction = ARROW_UP;
             loop {
                 if keyboard.states[position].next[direction] != ARROW_INVALID {
                     pending[pending_index] =
@@ -394,13 +390,13 @@ const fn get_shortest_path_between_numbers(start: usize, end: usize) -> [usize; 
     loop {
         let position = pending[index].last();
         if position == end {
-            return pending[pending_index].directions;
+            return pending[index].directions;
         }
 
         if !seen[position] {
             seen[position] = true;
 
-            let mut direction = 1;
+            let mut direction = ARROW_UP;
             loop {
                 if keyboard.states[position].next[direction] != ARROW_INVALID {
                     pending[pending_index] =
@@ -421,53 +417,91 @@ const fn get_shortest_path_between_numbers(start: usize, end: usize) -> [usize; 
     }
 }
 
+struct Positions {
+    numeric: usize,
+    arrow_1: usize,
+    arrow_2: usize,
+}
+
+impl Positions {
+    fn new() -> Self {
+        return Positions {
+            numeric: NUMERIC_A,
+            arrow_1: ARROW_A,
+            arrow_2: ARROW_A,
+        };
+    }
+
+    fn press_numeric_button(&mut self, target: usize) -> u64 {
+        let path = SHORTEST_NUMERIC_PATHS[self.numeric][target];
+        // println!(
+        //     "press_numeric_button from {} to {target} = {:?}",
+        //     self.numeric, path
+        // );
+        let mut output = 0;
+        let mut i = 0;
+        loop {
+            if path[i] == ARROW_INVALID {
+                break;
+            }
+            // TODO I don't think we need the state of arrow_1 or arrow_2.
+            output += self.press_arrow_1(path[i]);
+            output += self.press_arrow_1(ARROW_A);
+            i += 1;
+        }
+        self.numeric = target;
+        return output;
+    }
+
+    fn press_arrow_1(&mut self, target: usize) -> u64 {
+        let path = SHORTEST_ARROW_PATHS[self.arrow_1][target];
+        let mut output = 0;
+        let mut i = 0;
+        loop {
+            if path[i] == ARROW_INVALID {
+                break;
+            }
+            output += self.press_arrow_2(path[i]);
+            output += self.press_arrow_2(ARROW_A);
+            i += 1;
+        }
+        self.arrow_1 = target;
+        return output;
+    }
+
+    fn press_arrow_2(&mut self, target: usize) -> u64 {
+        let path = SHORTEST_ARROW_PATHS[self.arrow_2][target];
+        let mut i = 0;
+        loop {
+            if path[i] == ARROW_INVALID {
+                self.arrow_2 = target;
+                return i as u64;
+            }
+            i += 1;
+        }
+    }
+}
+
 #[aoc(day21, part1)]
 pub fn part1(input: &str) -> u64 {
     let input = input.as_bytes();
-    let mut i: usize = 1;
+    let mut i: usize = 0;
 
     let mut total = 0;
     let mut total_sequence_length = 0;
     let mut current_value = 0;
 
-    loop {
-        current_value *= 10;
-        current_value += (input[i - 1] - b'0') as u64;
-        let start = convert_to_number(input[i - 1]);
-        let end = convert_to_number(input[i]);
+    let mut positions = Positions::new();
 
-        let robot1 = SHORTEST_NUMERIC_PATHS[start][end];
-        let mut j = 1;
-        loop {
-            if robot1[j] == ARROW_INVALID {
-                break;
-            }
-            let start = robot1[j - 1];
-            let end = robot1[j];
-            let robot2 = SHORTEST_KEYBOARD_PATHS[start][end];
-            let mut k = 1;
-            loop {
-                if robot2[k] == ARROW_INVALID {
-                    break;
-                }
-                let start = robot2[k - 1];
-                let end = robot2[k];
-                let me = SHORTEST_KEYBOARD_PATHS[start][end];
-                let mut l = 1;
-                loop {
-                    if me[l] == ARROW_INVALID {
-                        total_sequence_length += (l - 1) as u64;
-                        break;
-                    }
-                    l += 1;
-                }
-                k += 1;
-            }
-            j += 1;
+    loop {
+        if input[i] < b'A' {
+            current_value *= 10;
+            current_value += (input[i] - b'0') as u64;
         }
+        total_sequence_length += positions.press_numeric_button(convert_to_number(input[i]));
         i += 1;
         if i >= input.len() || input[i] == b'\n' {
-            println!("total += {current_value} * {total_sequence_length}");
+            // println!("total += {current_value} * {total_sequence_length}");
             total += current_value * total_sequence_length;
 
             if i >= input.len() {
@@ -519,16 +553,36 @@ mod test {
     }
 
     fn get_example_input() -> &'static str {
-        return "";
+        return "029A
+980A
+179A
+456A
+379A";
+    }
+
+    #[test]
+    fn constant_paths() {
+        assert_eq!(
+            SHORTEST_ARROW_PATHS[ARROW_UP][ARROW_A],
+            [
+                ARROW_RIGHT,
+                ARROW_INVALID,
+                ARROW_INVALID,
+                ARROW_INVALID,
+                ARROW_INVALID,
+                ARROW_INVALID,
+            ]
+        )
     }
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&get_example_input()), 1)
+        assert_eq!(part1(&get_example_input()), 126384)
     }
 
     #[test]
     fn part1_real_input() {
+        // 125252 is too low.
         assert_eq!(part1(&get_input()), 1)
     }
 
