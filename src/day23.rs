25e05a1098aea08956c513a6a7a9c2ecefd45619
Josh Przybyszewski <josh.prezby@gmail.com>
2024-12-23 16:05:49 -0600

    solve the example

--- src/day23.rs
+++ src/day23.rs
@@ -1,3 +1,5 @@
+use std::collections::{hash_set::Union, HashSet};
+
 const MIN_T_ID: u16 = (b't' - b'a') as u16 * 26;
 const MAX_T_ID: u16 = (b'u' - b'a') as u16 * 26;
 
@@ -53,19 +55,90 @@ impl Graph {
     }
 
     fn solve_part2(&self) -> String {
-        let mut best: Vec<usize> = Vec::new();
-        // hehe TODO
-        best.push(0);
+        let best = self.get_maximal_complete_subgraph();
+        let mut ids = best
+            .iter()
+            .map(|index| self.nodes[*index].id)
+            .collect::<Vec<u16>>();
+        ids.sort();
 
         let num_connected = best.len();
         let mut array: [u8; 3 * 5000] = [b','; 3 * 5000];
         for i in 0..num_connected {
-            let node_index = best[i];
-            let node_id = self.nodes[node_index].id;
+            // let node_index = best[i];
+            let node_id = ids[i];
             array[i * 3] = b'a' + (node_id / 26) as u8;
             array[i * 3 + 1] = b'a' + (node_id % 26) as u8;
         }
         return String::from_utf8_lossy(&array[0..(num_connected * 3) - 1]).to_string();
+    }
+
+    fn get_maximal_complete_subgraph(&self) -> Vec<usize> {
+        return self
+            .bron_keybosch1(
+                HashSet::new(),
+                HashSet::from_iter(0..self.nodes.len()),
+                HashSet::new(),
+            )
+            .iter()
+            .map(|index| *index)
+            .collect::<Vec<usize>>();
+    }
+
+    // https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+    // algorithm BronKerbosch1(R, P, X) is
+    // if P and X are both empty then
+    //     report R as a maximal clique
+    // for each vertex v in P do
+    //     BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+    //     P := P \ {v}
+    //     X := X ⋃ {v}
+    fn bron_keybosch1(
+        &self,
+        r: HashSet<usize>,
+        p: HashSet<usize>,
+        x: HashSet<usize>,
+    ) -> HashSet<usize> {
+        if p.is_empty() {
+            if x.is_empty() {
+                return r.clone();
+            }
+            return HashSet::new();
+        }
+
+        let mut x = x.clone();
+        let mut best: HashSet<usize> = HashSet::new();
+
+        for v in p.iter() {
+            // For each vertex v chosen from P, it makes a recursive call
+            // in which v is added to R and in which P and X are restricted
+            // to the neighbor set N(v) of v, which finds and reports all
+            // clique extensions of R that contain v. Then, it moves v from
+            // P to X to exclude it from consideration in future cliques and
+            // continues with the next vertex in P.
+            let r_union_v =
+                HashSet::from_iter(r.union(&HashSet::from([*v])).into_iter().map(|e| *e));
+            let v_neighbors: HashSet<usize> =
+                HashSet::from_iter(self.nodes[*v].others.iter().map(|e| *e));
+            let p_intersection_v_neighbors =
+                HashSet::from_iter(p.intersection(&v_neighbors).into_iter().map(|e| *e));
+            let x_intersection_v_neighbors =
+                HashSet::from_iter(x.intersection(&v_neighbors).into_iter().map(|e| *e));
+            let answer = self.bron_keybosch1(
+                r_union_v,
+                p_intersection_v_neighbors,
+                x_intersection_v_neighbors,
+            );
+            x.insert(*v);
+            if answer.is_empty() {
+                continue;
+            }
+            if best.len() < answer.len() {
+                best = answer;
+            }
+        }
+
+        return best;
     }
 
     fn solve_part1(&self) -> u16 {
