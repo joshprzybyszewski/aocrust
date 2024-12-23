7d60aa574bb88221e850df73099aad5a0e7d7a78
Josh Przybyszewski <josh.prezby@gmail.com>
2024-12-22 21:31:51 -0600

    consider part 2

--- src/day22.rs
+++ src/day22.rs
@@ -2,16 +2,22 @@
 // hex:     0x1000000
 // 25 binary digits = 0b1000000000000000000000000
 const MODULO_MASK: i32 = 0xFFFFFF;
+const NUM_ITERATIONS: usize = 2000;
 
-fn generate_times(secret: i32, num_times: usize) -> i32 {
+const fn generate_times(secret: i32) -> i32 {
     let mut val = secret;
-    for _ in 0..num_times {
+    let mut i = 0;
+    loop {
+        if i == NUM_ITERATIONS {
+            break;
+        }
         val = generate(val);
+        i += 1;
     }
     return val;
 }
 
-fn generate(secret: i32) -> i32 {
+const fn generate(secret: i32) -> i32 {
     // multiplying the secret number by 64
     // mix this result, then prune
     let secret = (secret ^ (secret << 6)) & MODULO_MASK;
@@ -31,7 +37,7 @@ pub fn part1(input: &str) -> u64 {
     let mut total: u64 = 0;
     loop {
         if input[i] == b'\n' {
-            total += generate_times(val, 2000) as u64;
+            total += generate_times(val) as u64;
             i += 1;
             val = 0;
         }
@@ -39,16 +45,47 @@ pub fn part1(input: &str) -> u64 {
         val += (input[i] - b'0') as i32;
         i += 1;
         if i >= input.len() {
-            total += generate_times(val, 2000) as u64;
+            total += generate_times(val) as u64;
             break;
         }
     }
     return total;
 }
 
+fn consider_part2(secret: i32) -> i32 {
+    let mut val = secret;
+    let mut i = 0;
+    loop {
+        if i == NUM_ITERATIONS {
+            break;
+        }
+        val = generate(val);
+        i += 1;
+    }
+    return val;
+}
+
 #[aoc(day22, part2)]
-pub fn part2(input: &str) -> u32 {
-    return 0;
+pub fn part2(input: &str) -> i32 {
+    let input = input.as_bytes();
+    let mut i: usize = 0;
+    let mut val = 0;
+    let mut total = 0;
+    loop {
+        if input[i] == b'\n' {
+            total += consider_part2(val);
+            i += 1;
+            val = 0;
+        }
+        val *= 10;
+        val += (input[i] - b'0') as i32;
+        i += 1;
+        if i >= input.len() {
+            total += consider_part2(val);
+            break;
+        }
+    }
+    return total;
 }
 
 #[cfg(test)]
@@ -69,13 +106,19 @@ mod test {
 2024";
     }
 
+    fn get_example_input_2() -> &'static str {
+        return "1
+2
+3
+2024";
+    }
+
     #[test]
     fn part1_example() {
         assert_eq!(generate(123), 15887950);
-        assert_eq!(generate_times(123, 1), 15887950);
-        assert_eq!(generate_times(123, 2), 16495136);
-        assert_eq!(generate_times(123, 3), 527345);
-        assert_eq!(generate_times(123, 4), 704524);
+        assert_eq!(generate(generate(123)), 16495136);
+        assert_eq!(generate(generate(generate(123))), 527345);
+        assert_eq!(generate(generate(generate(generate(123)))), 704524);
         assert_eq!(part1(&get_example_input()), 37327623);
     }
 
@@ -86,7 +129,7 @@ mod test {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&get_example_input()), 1);
+        assert_eq!(part2(&get_example_input_2()), 23);
     }
 
     #[test]
